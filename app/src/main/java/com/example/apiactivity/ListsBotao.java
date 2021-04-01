cd0b50dc48e4dfca0a09c7a6b17f99950a0e099c
LeonardoEckertMarques <leonardoeckert.contato@gmail.com>
2021-04-01 20:42:51 -0300

    Switch para botões lista

--- app/src/main/java/com/example/apiactivity/ListsBotao.java
+++ app/src/main/java/com/example/apiactivity/ListsBotao.java
@@ -7,25 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class ListsBotao extends AppCompatActivity {
-
-  private Button comments;
+public class ListsBotao extends AppCompatActivity implements View.OnClickListener {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_lista_botoes);
-
-    comments = findViewById(R.id.comments);
-
-    comments.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Intent i = new Intent(ListsBotao.this, ListsActivity.class);
-        i.putExtra("op", "comments");
-        startActivity(i);
-      }
-    });
-
+    Button comments = findViewById(R.id.comments);
+    comments.setOnClickListener(this);
   }
+
+  // NO EXÉRCICÍO ANTERIOR EU NÃO ACABEI REALIZANDO UM SWITCH PERCORRENDO OS BOTÕES....
+  // MAS ESTÁ AÍ AGORA....
+  public void onClick(View view) {
+    Intent intent = new Intent(ListsBotao.this, ListsActivity.class);
+    switch (view.getId()) {
+      case R.id.comments:
+        intent.putExtra("op", "comments");
+        startActivity(intent);
+        break;
+    }
+  }
+
 }
